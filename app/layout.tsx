c3940c1cb1e953eeea9a64b326eee3675337d834
Luca <luca@example.com>
2026-05-29 22:55:03 +0200

    feat: update fonts, colors, and layout with Calendas and Overused Grotesk

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import { Playfair_Display } from "next/font/google"
+import localFont from "next/font/local"
+
+import "./globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@/lib/utils"
+
+const calendas = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-calendas",
+  weight: "500",
+})
+
+const overusedGrotesk = localFont({
+  src: "./fonts/OverusedGrotesk-Book.woff2",
+  variable: "--font-overused-grotesk",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn(
+        "antialiased",
+        calendas.variable,
+        overusedGrotesk.variable,
+        "font-overusedGrotesk"
+      )}
+    >
+      <body>
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
